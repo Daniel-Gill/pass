7f0a8294758b453c14b67090158fe44ea709a3c7
Daniel-Gill <d.gill@se20.qmul.ac.uk>
2022-03-23 13:25:22 +0000

    Added array lists

--- src/main/java/net/danielgill/pass/Main.java
+++ src/main/java/net/danielgill/pass/Main.java
@@ -1,5 +1,7 @@
 package net.danielgill.pass;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Student s = new Student("Bob", 6267582);
@@ -8,5 +10,12 @@ public class Main {
         
         Student s2 = registry.getStudentById(6267582);
         System.out.println(s2.getName());
+
+        ArrayList<Integer> ints = new ArrayList<>();
+        ArrayList<Boolean> booleans = new ArrayList<>();
+        booleans.add(true);
+        booleans.add(false);
+        booleans.add(true);
+        System.out.println(booleans);
     }
 }
